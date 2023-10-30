580338078fb405cc11712932697bc857ea7a3e3a
stevensillsii <stevensills2@gmail.com>
2023-10-29 23:36:01 -0700

    got the server to connect

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const mysql = require("mysql2");
 // connect to MySQL
 const connection = mysql.createConnection({
     host: "localhost",
-    port: 3001,
+    port: 3306,
     user: "root",
-    password: password",
-    database: "employeeTracker_db",
+    password: "password",
+    database: "eTrack_db",
 });
 
 // connect to the database
@@ -15,5 +15,5 @@ connection.connect((err) => {
     if (err) throw err;
     console.log("Connected to the database!");
     // start the application
-    start();
+//     start();
 });
